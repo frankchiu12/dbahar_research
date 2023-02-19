01872a25add95f5e0487864bc2041f4b3342a735
frankchiu12 <si-un_chiu@brown.edu>
2023-02-19 18:02:02 -0500

    try removing 0s

--- segment_by_country_tech.py
+++ segment_by_country_tech.py
@@ -1,16 +1,19 @@
-# import pandas as pd
-# import sys
+import pandas as pd
+import sys
 
-# reload(sys)
-# sys.setdefaultencoding('utf8')
+reload(sys)
+sys.setdefaultencoding('utf8')
 
-# df = pd.read_csv('/users/schiu4/data/schiu4/PatentsInventorsTimed.csv')
+df = pd.read_csv('/users/schiu4/data/schiu4/PatentsInventorsTimed.csv')
 
-# for country in df.inventor_iso2.unique():
-#     sub_df = df[df.inventor_iso2 == country]
-#     for technology in sub_df.cpc_id.unique():
-#         sub_sub_df = sub_df[sub_df.cpc_id == technology]
-#         cut_length = sub_sub_df.shape[0]/10
-#         sub_sub_df = sub_sub_df.sort_values(by=['pri_time'])
-#         sub_sub_df = sub_sub_df.head(cut_length)
-#         sub_sub_df.to_csv('/users/schiu4/data/schiu4/segmented_data/' + country + '-' + technology + '.csv')
+for country in df.inventor_iso2.unique():
+    sub_df = df[df.inventor_iso2 == country]
+    for technology in sub_df.cpc_id.unique():
+        sub_sub_df = sub_df[sub_df.cpc_id == technology]
+        cut_length = sub_sub_df.shape[0]/10
+        if cut_length == 0:
+            print('small')
+            continue
+        sub_sub_df = sub_sub_df.sort_values(by=['pri_time'])
+        sub_sub_df = sub_sub_df.head(cut_length)
+        sub_sub_df.to_csv('/users/schiu4/data/schiu4/segmented_data/' + country + '-' + technology + '.csv')
