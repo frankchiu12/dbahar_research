83fceff643bb4d967cb3a7eeb136764e0f572f34
frankchiu12 <si-un_chiu@brown.edu>
2023-02-20 13:07:25 -0500

    nice graph

--- network_x.py
+++ network_x.py
@@ -11,10 +11,12 @@ join_df = pd.read_csv(csv_url, usecols=['inventor_id', 'GMI1yr_prevexpabroad'])
 patent_to_inventor = {}
 inventor_to_patent = {}
 inventor_to_partner = {}
+inventor_to_indicator = {}
 
 inventor_id_list = []
 global_partner_list = []
 partner_id_count_list = []
+color_list = []
 deg_centrality_list = []
 close_centrality_list = []
 bet_centrality_list = []
@@ -28,11 +30,14 @@ for index, row in df.iterrows():
         patent_to_inventor[patent_id] = [inventor_id]
     else:
         patent_to_inventor[patent_id].append(inventor_id)
-    
+
     if inventor_id not in inventor_to_patent:
         inventor_to_patent[inventor_id] = [patent_id]
     else:
         inventor_to_patent[inventor_id].append(patent_id)
+    
+    if inventor_id not in inventor_to_indicator:
+        inventor_to_indicator[inventor_id] = row['GMI1yr_prevexpabroad']
 
 for inventor, patent in inventor_to_patent.items():
     inventor_id_list.append(inventor)
@@ -57,8 +62,14 @@ for inventor, partner in inventor_to_partner.items():
         g.add_node(part)
         g.add_edge(inventor, part)
 
+for node in g:
+    if inventor_to_indicator[node] == 1:
+        color_list.append('red')
+    else:
+        color_list.append('blue')
+
 plt.figure(figsize=(30,25))
-nx.draw(g, pos=nx.spiral_layout(g), node_size=100, edge_color=['red', 'green'], linewidths=10)
+nx.draw(g, pos=nx.spiral_layout(g), node_size=100, node_color=color_list, edge_color=['green'], linewidths=10)
 plt.savefig('graph.png')
 
 # random or spiral
