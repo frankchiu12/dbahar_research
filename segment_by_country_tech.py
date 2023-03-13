ebb64b104b814ec4307ba07b427f0baa7f828d38
frankchiu12 <si-un_chiu@brown.edu>
2023-03-13 14:06:32 -0400

    works?

--- segment_by_country_tech.py
+++ segment_by_country_tech.py
@@ -11,9 +11,14 @@ for country in df.inventor_iso2.unique():
     for technology in sub_df.cpc_id.unique():
         sub_sub_df = sub_df[sub_df.cpc_id == technology]
         cut_length = sub_sub_df.shape[0]/10
-        if cut_length == 0:
-            continue
-        else:
-            sub_sub_df = sub_sub_df.sort_values(by=['time'])
-            sub_sub_df = sub_sub_df.head(cut_length)
-            sub_sub_df.to_csv('/gpfs/home/schiu4/segmented_data/' + country + '-' + technology + '.csv')
+        decile = 0
+        for i in range(10):
+            cut_df = sub_sub_df.head(cut_length * (i + 1))
+            if 1 in cut_df['GMI1yr_prevexpabroad'].values:
+                decile = i + 1
+                break
+        sub_sub_df = sub_sub_df.sort_values(by=['time'])
+        sub_sub_df = sub_sub_df.head(cut_length)
+        sub_sub_df['decile'] = decile
+        sub_sub_df.to_csv('/gpfs/home/schiu4/segmented_data/' + country + '-' + technology + '.csv')
+        # sub_sub_df.to_csv('/gpfs/home/schiu4/segmented_data_test/' + country + '-' + technology + '.csv')
