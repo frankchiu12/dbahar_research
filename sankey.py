988721cc252b3fb94329382dc1f94dd5416406a9
frankchiu12 <si-un_chiu@brown.edu>
2023-03-16 22:04:22 -0400

    sankey

---
+++ sankey.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+df = pd.read_csv('data/Rank_data.csv')
+dictionary = {}
+to_print_list = []
+
+for i, row in df.iterrows():
+    pair = row['pair']
+    source = pair.partition('-')[0]
+    destination = pair.partition('-')[2]
+    tot_GMIs = row['totGMIs']
+    if source not in dictionary:
+        dictionary[source] = {}
+    if destination not in dictionary[source]:
+        dictionary[source][destination] = tot_GMIs
+    else:
+        dictionary[source][destination] += tot_GMIs
+
+print(dictionary)
+
+for key, value in dictionary.items():
+    for sub_key, sub_value in dictionary[key].items():
+        to_print_list.append([key, sub_key, sub_value])
+
+print(to_print_list)
