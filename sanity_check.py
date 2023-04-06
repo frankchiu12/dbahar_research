e8ca1b04204938eafa33f43edf7fcb7367226a09
frankchiu12 <si-un_chiu@brown.edu>
2023-04-06 17:21:55 -0400

    code finished

--- sanity_check.py
+++ sanity_check.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 df = pd.read_csv('data/sanity_check.csv')
-print(df.index[df['patent_id'] == 5448317].tolist())
+print(df.index[df['patent_id'] == 10151509].tolist())
 print(df['patent_id'].nunique())
 print(len(df))
--- segment_by_country_firm.py
+++ segment_by_country_firm.py
@@ -4,29 +4,34 @@ import math
 df = pd.read_csv('/gpfs/home/schiu4/PatentsInventorsTimed.csv')
 no_GMI_df = pd.DataFrame()
 country_list = []
-firm_list = []
+tech_list = []
 
 for country in df.inventor_iso2.unique():
     sub_df = df[df.inventor_iso2 == country]
-    for firm in sub_df.assigneeid.unique():
-        sub_sub_df = sub_df[sub_df.assigneeid == firm]
-        sub_sub_df = sub_sub_df.sort_values(by=['time'])
-        cut_length = sub_sub_df.shape[0] / 10
-        decile = 0
-        for i in range(10):
-            cut_df = sub_sub_df.head(int(math.floor(cut_length * (i + 1))))
-            if 1 in cut_df['GMI1yr_prevexpabroad'].values:
-                decile = i + 1
-                break
+    for tech in sub_df.cpc_id.unique():
+        sub_sub_df = sub_df[sub_df.cpc_id == tech]
+        sub_sub_df['time'] = pd.to_datetime(sub_sub_df['time'])
+        sub_sub_df = sub_sub_df.sort_values(by=['time'], ignore_index=True)
+        unique_firm_df = sub_sub_df.drop_duplicates(subset='assigneeid', keep='first', inplace=False).reset_index(drop=True)
+        unique_firm_df = unique_firm_df.sort_values(by=['time'], ignore_index=True)
+        if len(unique_firm_df) < 10:
+            continue
+        if len(sub_sub_df[sub_sub_df.GMI1yr_prevexpabroad == 1]) == 0:
+            country_list.append(country)
+            tech_list.append(tech)
+            continue
+        first_GMI_firm = sub_sub_df[sub_sub_df.GMI1yr_prevexpabroad == 1].iloc[0]['assigneeid']
+        index = int(unique_firm_df.index[unique_firm_df.assigneeid == first_GMI_firm][0])
+        decile = math.ceil((index/len(unique_firm_df)) * 10)
+        if decile == 0:
+            decile = 1
+        firms_to_consider = unique_firm_df.head(math.ceil(decile/10 * len(unique_firm_df))).assigneeid.unique()
+        cut_df = sub_sub_df[~sub_sub_df.assigneeid.isin(firms_to_consider)].reset_index()
         cut_df['decile'] = decile
         if len(cut_df.index) == 0:
             continue
-        if decile == 0:
-            country_list.append(country)
-            firm_list.append(firm)
-            continue
-        cut_df.to_csv('/gpfs/home/schiu4/segmented_data_firm/' + country + '-' + str(int(firm)) + '.csv')
+        cut_df.to_csv('/gpfs/home/schiu4/segmented_data_firm/' + country + '-' + tech + '.csv')
 
 no_GMI_df['country'] = country_list
-no_GMI_df['firm'] = firm_list
+no_GMI_df['tech'] = tech_list
 no_GMI_df.to_csv('/gpfs/home/schiu4/no_GMI_firm.csv')
