3ef1c3e38a35fe66c3b0c426556e0a6a304a6844
frankchiu12 <si-un_chiu@brown.edu>
2023-04-06 12:31:30 -0400

    code finished

--- sanity_check.py
+++ sanity_check.py
@@ -8,9 +8,8 @@ def check(step):
         df.to_csv('/gpfs/home/schiu4/sanity_check.csv')
     if step == 2:
         df = pd.read_csv('data/sanity_check.csv')
-        df = df.sort_values(by=['time'])
-        df.reindex(list(range(0, len(df))))
-        df.to_csv('data/sanity_check.csv')
+        df = df.sort_values(by=['time'], ignore_index=True)
+        df.to_csv('data/sanity_check.csv', columns=['patent_id', 'inventor_id', 'GMI1yr_prevexpabroad','inventor_iso2', 'cpc_id', 'location_id', 'assigneeid', 'time'])
         print(df.index[df['GMI1yr_prevexpabroad'] == 1].tolist())
         print(len(df))
 
--- segment_by_country_tech.py
+++ segment_by_country_tech.py
@@ -10,20 +10,25 @@ for country in df.inventor_iso2.unique():
     sub_df = df[df.inventor_iso2 == country]
     for tech in sub_df.cpc_id.unique():
         sub_sub_df = sub_df[sub_df.cpc_id == tech]
-        sub_sub_df = sub_sub_df.sort_values(by=['time'])
-        cut_length = sub_sub_df.shape[0] / 10
-        decile = 0
-        for i in range(10):
-            cut_df = sub_sub_df.head(math.floor(cut_length * (i + 1)))
-            if 1 in cut_df['GMI1yr_prevexpabroad'].values:
-                decile = i + 1
-                break
-        cut_df['decile'] = decile
-        if len(cut_df.index) == 0:
+        sub_sub_df = sub_sub_df.sort_values(by=['time'], ignore_index=True)
+        unique_patent_df = sub_sub_df.drop_duplicates(subset='patent_id', keep='first', inplace=False)
+        if len(unique_patent_df) < 10:
             continue
-        if decile == 0:
+        if len(sub_sub_df[sub_sub_df.GMI1yr_prevexpabroad == 1]) == 0:
             country_list.append(country)
             tech_list.append(tech)
+            continue
+        first_patent = sub_sub_df[sub_sub_df.GMI1yr_prevexpabroad == 1].iloc[0]['patent_id']
+        index = int(df.index[df.patent_id == first_patent][0])
+        decile = math.ceil((index/len(unique_patent_df)) * 10)
+        patents_to_consider = df.head(math.ceil(decile/10 * len(unique_patent_df))).patent_id.unique()
+        patent_list = sub_sub_df.patent_id.to_list()
+        index = 0
+        for i, patent in enumerate(patent_list):
+            if patent in patents_to_consider:
+                index = i
+        cut_df = sub_sub_df.head(index + 1)
+        if len(cut_df.index) == 0:
             continue
         cut_df.to_csv('/gpfs/home/schiu4/segmented_data_tech/' + country + '-' + tech + '.csv')
 
