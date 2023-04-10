c7a4c613b5fbbf5bbd7fb525022987e9a4413630
frankchiu12 <si-un_chiu@brown.edu>
2023-04-10 15:58:05 -0400

    code finished

--- clean_folder.py
+++ clean_folder.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-path = '/gpfs/home/schiu4/segmented_data_patent/'
+path = '/gpfs/home/schiu4/segmented_data_patent_decile/'
 
 for x in os.listdir(path):
     file = os.path.join(path, x)
