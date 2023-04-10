a046ca3f9a1b11d9d8c9dbd690e238657963ad11
frankchiu12 <si-un_chiu@brown.edu>
2023-04-10 19:27:35 -0400

    code finished

--- sanity_check.py
+++ sanity_check.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
-df = pd.read_csv('data/sanity_check.csv')
-print(df.index[df['patent_id'] == 10151509].tolist())
-print(df['patent_id'].nunique())
-print(len(df))
+df_1 = pd.read_csv('data/CumulativeDataPatent.csv')
+df_2 = pd.read_csv('data/Tech_class_decile 1st GMI.csv', usecols=['country', 'technology', 'decile_1stGMI_Sara'])
+
+merged_df = pd.merge(df_1, df_2, on=['country', 'technology'])
+merged_df = merged_df.drop(['Unnamed: 0'], axis=1)
+merged_df.to_csv('data/sanity_check.csv')
