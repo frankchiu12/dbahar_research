4663d1a56a400b211636589259a7d03e20d15599
frankchiu12 <si-un_chiu@brown.edu>
2023-04-10 15:55:13 -0400

    code finished

--- convert_to_datetime.py
+++ convert_to_datetime.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('/gpfs/home/schiu4/PatentsInventorsDroppedNAN.csv', dtype={'year': str, 'month': str, 'day': str, 'GMI1yr_prevexpabroad': str})
+df = pd.read_csv('/gpfs/home/schiu4/PatentInventorsDeciles.csv', dtype={'year': str, 'month': str, 'day': str, 'GMI1yr_prevexpabroad': str})
 
 def drop_decimal(string_to_drop):
 	return string_to_drop.partition('.0')[0]
@@ -15,6 +15,6 @@ df = df[df.month != '0']
 df = df[df.day != '0']
 df['time'] = df['year'] + '/' + df['month'] + '/' + df['day']
 
-df = df.drop(['year', 'month', 'day', 'Unnamed: 0.1', 'Unnamed: 0'], axis=1)
+df = df.drop(['year', 'month', 'day', 'GMI', 'GMI1yr', 'GMIalways','GMI_prevexp', 'GMI1yr_prevexp', 'GMIalways_prevexp', 'GMI_prevexpabroad', 'GMIalways_prevexpabroad', 'pt1', 'pt2', 'pt3', 'pt4', 'pt5', 'pt6', 'pt7', 'pt8', 'pt9', 'pt10', 'class', 'tot_patents', 'patentcount', 'pt_patents'], axis=1)
 
-df.to_csv('/gpfs/home/schiu4/PatentsInventorsTimed.csv', index=False)
+df.to_csv('/gpfs/home/schiu4/PatentInventorsDecilesTimed.csv', index=False)
