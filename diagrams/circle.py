62df80a136f91b063164126b342316c0025be7b1
frankchiu12 <si-un_chiu@brown.edu>
2023-04-25 11:51:05 -0400

    code finished

--- diagrams/circle.py
+++ diagrams/circle.py
@@ -1,20 +1,17 @@
 import pandas as pd
-import holoviews as hv
-from holoviews import opts, dim
+from pycirclize import Circos
+from pycirclize.parser import Matrix
 import math
-from bokeh.plotting import show
 
-hv.extension('bokeh')
-
-df = pd.read_csv('data/CircularPlotTop5020152019.csv')
-new_df = pd.DataFrame()
+data_df = pd.read_csv('data/CircularPlotTop5020152019.csv')
+matrix_df = pd.DataFrame()
 
 source_target_value_list = []
 source_list = []
 target_list = []
 value_list = []
 
-for _, row in df.iterrows():
+for _, row in data_df.iterrows():
     if not math.isnan(row['movedAT']):
         source_target_value_list.append((row['prev_iso2'], 'AT', row['movedAT']))
     if not math.isnan(row['movedAU']):
@@ -59,29 +56,19 @@ for i, source_target_value in enumerate(source_target_value_list):
     target_list.append(source_target_value[1])
     value_list.append(source_target_value[2])
 
-new_df['source'] = source_list
-new_df['target'] = target_list
-new_df['value'] = value_list
+matrix_df['source'] = source_list
+matrix_df['target'] = target_list
+matrix_df['value'] = value_list
 
-node = pd.DataFrame()
-for i, value in enumerate(new_df.source.unique()):
-    _list = list(new_df[new_df['source'] == value].source.unique())
-    node = pd.concat([node, pd.DataFrame({'name':_list})], ignore_index=True)
+matrix = Matrix.parse_fromto_table(matrix_df)
 
-values = list(new_df.source.unique())
-d = {value: i for i, value in enumerate(values)}
-
-def str2num(s):
-    return d[s]
-
-new_df.source = new_df.source.apply(str2num)
-new_df.target = new_df.target.apply(str2num)
-
-hv.Chord(new_df)
-nodes = hv.Dataset(pd.DataFrame(node), 'index')
-
-chord = hv.Chord((new_df, nodes)).select(value=(5, None))
-chord.opts(opts.Chord(cmap='Category20', edge_cmap='Category20', edge_color=dim('target').str(), labels='name', node_color=dim('index').str()))
-chord.opts(label_text_font_size='7pt', width=1000, height=1000, tools=['save', 'pan', 'wheel_zoom', 'box_zoom', 'reset'])
-
-show(hv.render(chord))
+circos = Circos.initialize_from_matrix(
+    matrix,
+    space=3,
+    cmap='tab20',
+    ticks_interval=250,
+    label_kws=dict(size=12, r=110),
+    link_kws=dict(direction=1, ec='black', lw=0.5),
+)
+fig = circos.plotfig()
+fig.savefig('diagrams/circle_50.png')
