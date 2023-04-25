1d7499330914a59ff5ffbfbd6ec5da4e42a73b66
frankchiu12 <si-un_chiu@brown.edu>
2023-04-25 11:17:49 -0400

    code finished

--- sanity_check.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df_1 = pd.read_csv('data/CumulativeDataPatent.csv')
-df_2 = pd.read_csv('data/Tech_class_decile 1st GMI.csv', usecols=['country', 'technology', 'decile_1stGMI_Sara'])
-
-merged_df = pd.merge(df_1, df_2, on=['country', 'technology'])
-merged_df = merged_df.drop(['Unnamed: 0'], axis=1)
-merged_df.to_csv('data/sanity_check.csv')
